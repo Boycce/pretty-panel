1e6f8c8ec52957eee47a72e6ea3f22a80b938daf
Ricky Boyce <boycce@gmail.com>
2015-03-23 14:37:00 +1300

    Added c::set('hidePages') to hide pages in sidebar.

--- app/snippets/pages/sidebar/subpage.php
+++ app/snippets/pages/sidebar/subpage.php
@@ -1,6 +1,14 @@
+<?php 
+  foreach (c::get('hidePages') as $url) 
+    if ($url == $subpage->slug()) $ignorepage = true;
+  if (!isset($ignorepage)):
+?>
+
 <li <?php if ($subpage->uid() == 'error') echo 'class="error"' ?>>
   <a class="draggable" data-helper="<?php echo esc($subpage->title(), 'attr') ?>" data-text="<?php echo esc(dragText($subpage), 'attr') ?>" href="<?php _u($subpage, 'show') ?>">
     <?php i($subpage) ?><span><?php __($subpage->uid()) ?></span>
     <small class="marginalia shiv shiv-left shiv-white"><?php __(n($subpage)) ?></small>
   </a>
 </li>
+
+<?php endif ?>
